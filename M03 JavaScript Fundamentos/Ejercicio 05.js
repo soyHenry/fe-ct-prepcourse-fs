2f8bb84fb18e481a0616b0b73582787cb3400cdb
UsuarioDeGithub <tu@correo.com>
2023-01-17 23:12:17 -0300

    change the word rectangle to square

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -34,7 +34,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 }
 
 function retornarPerimetro(lado) {
-   // La función recibe como argumento la medida de un lado de un rectángulo.
+   // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
 }
