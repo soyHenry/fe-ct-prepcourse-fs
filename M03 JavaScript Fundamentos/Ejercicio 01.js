a09857097e935a0fe06a758a6fe40d4654faa9a3
AlejoBengo <alejobengo@gmail.com>
2023-01-24 19:06:00 -0300

    Mejorando explicación

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -1,6 +1,9 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*1️⃣ EJERCICIO 01 1️⃣*/
 
+// En los siguientes ejercicios deberás reemplazar el valor null
+// por el correspondiente.
+
 // Crea una variable de tipo string.
 const nuevoString = null;
 
