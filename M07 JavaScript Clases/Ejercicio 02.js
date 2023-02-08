5eae3d15f9a4c74a7e5f718bb1b6c1c83c6b02b9
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-02-08 17:25:28 -0300

    Feature:Functionality added to functions
    Homework 07-Classes|part2 finished without tests errors

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -5,21 +5,40 @@ class Persona {
    // Crea el constructor de la clase "Persona".
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
-   // sus valores.
+   //sus valores
+   constructor(nombre, apellido, edad, domicilio) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+   }
+
+   detalle() {
+      return ({
+         nombre: this.nombre,
+         apellido: this.apellido,
+         edad: this.edad,
+         domicilio: this.domicilio,
+      })
+   }
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
-   // Tu código:
+   
+   return persona = new Persona(nombre, apellido, edad, domicilio);
 }
 
 function agregarMetodo() {
    // La función agrega un método "datos" a la clase "Persona".
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
-   // Tu código:
+   
+   Persona.prototype.datos = function() {
+      return (`${this.nombre}, ${this.edad} años`);
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
