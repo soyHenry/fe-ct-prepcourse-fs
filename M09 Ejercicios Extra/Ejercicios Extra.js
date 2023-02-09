a0f4e9822578afea0be491ebad9a4f4323d62256
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-02-09 14:58:56 -0300

    Feature:Functionality was added to all functions
    Homework-M09-Extra | Finished without test errors

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -5,7 +5,8 @@ function deObjetoAarray(objeto) {
    // Cada elemento del arreglo padre será un nuevo arreglo que contendrá dos elementos.
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
-   // Tu código:
+
+   return Object.entries(objeto);
 }
 
 function numberOfCharacters(string) {
@@ -13,34 +14,81 @@ function numberOfCharacters(string) {
    // letras del string, y su valor es la cantidad de veces que se repite en el string.
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
-   // Tu código:
+
+   const newObject = {};
+   for (letter of string) {
+      if (Object.hasOwn(newObject, letter)) {
+         ++newObject[letter]
+      } else {
+         newObject[letter] = 1;
+      }
+   }
+
+   return newObject;
 }
 
 function capToFront(string) {
    // Recibes un string con algunas letras en mayúscula y otras en minúscula.
    // Debes enviar todas las letras en mayúscula al comienzo del string.
    // Retornar el string.
-   // [EJEMPLO]: soyHENRY ---> HENRYsoy
-   // Tu código:
+   // [EJEMPLO]: soyHENRY ---> HENRYsoy   
+
+   const upperCaseSubString = string.match(/[A-Z]/g);
+   const lowerCaseSubString = string.match(/[a-z]/g);
+
+   return (upperCaseSubString.join("") + lowerCaseSubString.join(""));
 }
 
 function asAmirror(frase) {
    // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
-   // Tu código:
+
+   const fraseArray = frase.split(" ");
+   const newString = [];
+
+   for (word of fraseArray) {
+      const reverseWord = [];
+      for (let i = word.length; i >= 0; i--){
+         reverseWord.push(word[i]);
+      }
+      newString.push(reverseWord.join(""));
+   }
+
+   return newString.join(" ");
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
-   // Tu código:
+
+   const stringArray = numero.toString().split("");
+   let i = 0;
+   let j = (stringArray.length - 1);
+   let isCapicua = true;
+
+   while (i < j) {
+      if (stringArray[i] !== stringArray[j]) {
+         isCapicua = false;
+      }
+      i++;
+      j--;
+   }
+
+   if (isCapicua) {
+      return ("Es capicua");
+   }
+   return ("No es capicua");
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
-   // Tu código:
+   
+   const regex = /[a-c]/g;
+   let newString = "";
+   newString = string.replace(regex, "");
+   return newString;
 }
 
 function sortArray(arrayOfStrings) {
@@ -48,7 +96,18 @@ function sortArray(arrayOfStrings) {
    // Debe retornar un nuevo arreglo, pero con las palabras ordenadas en orden creciente a partir
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
-   // Tu código:
+   const sortedArray = arrayOfStrings.sort((a, b) => {
+      if (a.length < b.length) {
+         return -1;
+      }
+
+      if (a.length > b.length) {
+         return 1;
+      }
+
+      return 0;
+   });
+   return sortedArray;
 }
 
 function buscoInterseccion(array1, array2) {
@@ -57,7 +116,13 @@ function buscoInterseccion(array1, array2) {
    // [EJEMPLO]: [4,2,3] U [1,3,4] = [3,4].
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
-   // Tu código:
+  return result = array1.filter((item) => {
+      for (number of array2) {
+         if (number === item) {
+            return number;
+         }
+      }
+   })
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
