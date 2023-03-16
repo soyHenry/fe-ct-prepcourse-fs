e6f3c30297cabcf31f13c421183f823cab7417f6
David Fernando Briceño Galindo <d.bricenogalindo@gmail.com>
2023-03-16 00:12:20 -0500

    Recuperacion proceso
    Envio de ejercicios 1 y 2

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var resultado = x + y;
+   return resultado
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var resultado = x - y;
+   return resultado
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var resultado = x / y;
+   return resultado
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var resultado = x * y;
+   return resultado
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var resultado = x % y;
+   return resultado
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
