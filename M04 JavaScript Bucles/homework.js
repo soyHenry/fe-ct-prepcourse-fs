252e4eabf2ae6741bb725a12ec53e7f4e21a0be7
sebastianalfredodw <123831694+sebastianalfredodw@users.noreply.github.com>
2023-04-07 12:09:12 -0400

    Update homework.js

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -1,53 +1,112 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 
 function obtenerMayor(x, y) {
-   // "x" e "y" son números enteros.
-   // Retornar el número más grande.
-   // Si son iguales, retornar cualquiera de los dos.
-   // Tu código:
-}
+    // "x" e "y" son números enteros.
+    // Retornar el número más grande.
+    // Si son iguales, retornar cualquiera de los dos.
+    // Tu código:
+    if (x > y) {
+        console.log("El valor mas grande es X: ");
+    } else if
+        (y > x) {
+        console.log("El valor mas grande es Y: ");
+    }
+    else console.log("Son iguales");
+   }
+obtenerMayor(8, 5); // El valor mas grande es x
 
 function mayoriaDeEdad(edad) {
    // Determinar si la persona puede ingresar al evento según su edad.
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
-}
+   if (edad >= 18){
+            console.log("Allowed")
+        } else
+            console.log("Not Allowed")
+    }
+    mayoriaDeEdad(34); // Muestra el string "Allowed" al tener mayoriadeedad.
 
 function conection(status) {
-   // El argumento "status" representa el estado de conexión de un usuario.
-   // Si el estado es igual a 1, el usuario está "Online".
-   // Si el estado es igual a 2, el usuario está "Away".
-   // De lo contrario, presumimos que el usuario está "Offline".
-   // Retornar el estado de conexión del usuario.
-   // Tu código:
-}
+        // El argumento "status" representa el estado de conexión de un usuario.
+        // Si el estado es igual a 1, el usuario está "Online".
+        // Si el estado es igual a 2, el usuario está "Away".
+        // De lo contrario, presumimos que el usuario está "Offline".
+        // Retornar el estado de conexión del usuario.
+        // Tu código:\
+        if (status === 1){
+            console.log("Online")
+        }else if (status === 2){
+            console.log("Away")
+        }else 
+        console.log ("Offline")
+        return status; 
+    }
+    conection(1); // Al tener valor 1 conection, asume que esta conectado.
 
 function saludo(idioma) {
-   // Retornar un saludo en tres diferentes lenguajes:
-   // Si "idioma" es "aleman", devuelve "Guten Tag!".
-   // Si "idioma" es "mandarin", devuelve "Ni Hao!".
-   // Si "idioma" es "ingles", devuelve "Hello!".
-   // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
-   // Tu código:
-}
+        // Retornar un saludo en tres diferentes lenguajes:
+        // Si "idioma" es "aleman", devuelve "Guten Tag!".
+        // Si "idioma" es "mandarin", devuelve "Ni Hao!".
+        // Si "idioma" es "ingles", devuelve "Hello!".
+        // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
+        // Tu código:
+        if (idioma === "aleman"){
+            console.log("Guten Tag!")
+        } else if (idioma === "mandarin") {
+            console.log("Ni Hao!")
+        } else if (idioma === "ingles"){
+            console.log("Hello!")
+        } else {
+            console.log("Hola!")
+        }
+        return idioma;
+        
+     }
+     saludo("aleman"); // saluda en aleman y retorna valor de la funcion "idioma";
 
 function colors(color) {
-   // La función recibe un color. Retornar el string correspondiente:
-   // En caso que el color recibido sea "blue"   --> "This is blue".
-   // En caso que el color recibido sea "red"    --> "This is red".
-   // En caso que el color recibido sea "green"  --> "This is green".
-   // En caso que el color recibido sea "orange" --> "This is orange".
-   // Si no es ninguno de esos colores           --> "Color not found".
-   // IMPORTANTE: utilizar el statement SWITCH.
-   // Tu código:
+    // La función recibe un color. Retornar el string correspondiente:
+    // En caso que el color recibido sea "blue"   --> "This is blue".
+    // En caso que el color recibido sea "red"    --> "This is red".
+    // En caso que el color recibido sea "green"  --> "This is green".
+    // En caso que el color recibido sea "orange" --> "This is orange".
+    // Si no es ninguno de esos colores           --> "Color not found".
+    // IMPORTANTE: utilizar el statement SWITCH.
+    // Tu código:
+    switch (color) {
+        case 'blue':
+            console.log("This is blue");
+            break;
+        case 'red':
+            console.log("This is red");
+            break;
+        case 'green':
+            console.log("This is green");
+            break;
+        case 'orange':
+            console.log("This is orange");
+            break;
+        default:
+            console.log("Color not found");
+        }
+        return color;
 }
 
+colors("yellow"); // declara que el color no ha sido encontrado "Color not found" y retorna el color seleccionado.
+
 function esDiezOCinco(num) {
-   // Retornar true si "num" es 10 o 5.
-   // De lo contrario, retornar false.
-   // Tu código:
-}
+    // Retornar true si "num" es 10 o 5.
+    // De lo contrario, retornar false.
+    // Tu código:
+    if ( num === 10 || num === 5){
+        return true;
+    } else 
+    return false;
+ }
+
+ esDiezOCinco(10); // retorna que el valor es 'true' al terner la funcion el valor 10
+
 
 function estaEnRango(num) {
    // Retornar true si "num" es menor que 50 y mayor que 20.
