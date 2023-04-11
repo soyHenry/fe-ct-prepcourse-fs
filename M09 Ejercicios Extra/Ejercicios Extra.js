0f44ef341587bd51da1f7d48e11d40e119c51f00
juanjose <chambijuanjose05@gmail.com>
2023-04-11 20:01:46 -0300

    Homework M03, M04, M05 y M06 Hecho

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,11 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   nuevoArray = [];
+   for (let clave in objeto){
+      nuevoArray.push([clave,objeto[clave]]);
+   };return nuevoArray;
+
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +19,16 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   obj = {};
+   for(let i = 0;i < string.length;i++){
+      if(obj.hasOwnProperty(string[i])){/*-2- este se ejecuta en la sigueinte vuelta si es que la siguiente palabra */
+         obj[string[i]] = obj[string[i]] + 1;/*del string es la misma letra ya alamacenada, y si esta, a esta se le suma*/
+      }                                      /* un 1, sumando asi 2 palabras repetidas */
+      else{                                  
+         obj[string[i]] = 1;/*-1- se ejecuta primero ya que no hay ninguna propiedad en obj, y con este codigo se agrega */
+      }                     /*un valor a obj, y este al ser agregado vale 1, acompñado de la letra agregada */
+   };return obj;
+
 }
 
 function capToFront(string) {
@@ -22,8 +37,10 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   mayus = string.match(/[A-Z]/g);
+   minus = string.match(/[a-z]/g);
+   return resultado = mayus.concat(minus).join(""); 
 }
-
 function asAmirror(frase) {
    // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
    // La diferencia es que cada palabra estará escrita al inverso.
@@ -41,6 +58,7 @@ function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +67,7 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   
 }
 
 function buscoInterseccion(array1, array2) {
