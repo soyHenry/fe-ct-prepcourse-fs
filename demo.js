79d284ea2155209d816b8ee5bb4cad897f07ea7d
Martin Soler <martinsoler_91@outlook.com>
2023-06-14 21:19:58 -0300

    demo.js file deleted

--- demo.js
+++
@@ -1,8 +0,0 @@
-function obtenerSaludo(nombre) {
-  return `Hola ${nombre}!`;
-  // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
-  // Ejemplo: "Martin" ---> "Hola Martin!"
-  // Tu código:
-}
-
-console.log(obtenerSaludo("Martin"));
