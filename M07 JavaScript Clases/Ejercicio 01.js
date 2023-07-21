8a955eba575c19fe2d671c1f8085feee09a9c871
cega17 <cegael17@gmail.com>
2023-07-20 21:41:16 -0500

    resolviendo 07

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,18 +10,46 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
-}
-
+   
+   class Usuario {
+      constructor(usuario, nombre, email, password) {
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+      }
+    
+      saludar() {
+        return `Hola, mi nombre es ${this.nombre}`;
+      }
+    }
+   
+   
+    // Retornar la clase
+    return Usuario;
+   }
+    
+   
+    
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function () {
+      return "Hello World!";
+    };
+ 
+ 
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function () {
+      return this.split('').reverse().join('');
+    };
+    
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
@@ -29,4 +57,6 @@ module.exports = {
    crearUsuario,
    agregarMetodoPrototype,
    agregarStringInvertida,
+
 };
+
