6c73ed5c68eba04f167a946b8a9e5e6cef6df374
juan90000 <43899377+juan90000@users.noreply.github.com>
2023-08-01 23:25:00 -0500

    Update Ejercicio 02.js

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,34 +4,80 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
-}
+}            var stringrt = ("Este es un string");
+            return stringrt;
+         }
+
+         returntstring();
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
-}
+}            var x = 7;
+            var y = 5;
+            const sumando = (x + y).value;
+            var resultado = x+y;
+            return resultado;
+            // console.log(resultado);
+         }
+
+         suma();
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
-}
+}            var x = 7;
+            var y = 5;
+            const restando = (x - y).value;
+            var resultado = x-y;
+            return resultado;
+            // console.log(resultado);
+         }
+
+         resta();
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
-}
+}            var x = 7;
+            var y = 5;
+            const dividiendo = (x / y).value;
+            var resultado = x/y;
+            return resultado;
+            // console.log(resultado);
+         }
+
+         divide();
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
-}
+}         function multiplica(){
+            var x = 7;
+            var y = 5;
+            const multiplicando = (x * y).value;
+            var resultado = x*y;
+            return resultado;
+            // console.log(resultado);
+         }
+
+         multiplica();
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
-}
+}         function obtenerResto(){
+            var x = 7;
+            var y = 5;
+            const dividiendo = (x / y).value;
+            var resultado = (x%y);
+            return resultado;
+            console.log(obtenerResto);
+         }
+
+         obtenerResto();
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
