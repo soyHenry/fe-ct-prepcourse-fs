762bd144655ee4e64e6856b480c9f7e4c9a34def
nydsanchez <nydsanchez@gmail.com>
2023-08-18 17:33:04 -0600

    ejercicio 5 finalizado

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -61,6 +61,8 @@ function eliminarPropiedad(objeto, propiedad) {
    // El parámetro "propiedad" es una propiedad del objeto que recibes.
    // Debes eliminarla del objeto y retornarlo finalmente.
    // Tu código:
+   delete objeto[propiedad];
+   return objeto;
 }
 
 function tieneEmail(objetoUsuario) {
