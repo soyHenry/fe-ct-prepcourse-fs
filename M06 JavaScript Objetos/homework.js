7ba4799ee8648fe8628b93f77025230caf1865af
nydsanchez <nydsanchez@gmail.com>
2023-08-18 18:17:09 -0600

    ejercicio 7 completado

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -81,6 +81,11 @@ function tienePropiedad(objeto, propiedad) {
    // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   if(objeto.hasOwnProperty([propiedad])){
+      return true;
+   } else{
+      return false;
+   }
 }
 
 function verificarPassword(objetoUsuario, password) {
