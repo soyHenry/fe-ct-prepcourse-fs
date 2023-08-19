7a1d8db11e28c2d2b6777e2a77de44ffa6b808db
nydsanchez <nydsanchez@gmail.com>
2023-08-18 18:29:30 -0600

    ejercicio 8 terminado

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -92,6 +92,11 @@ function verificarPassword(objetoUsuario, password) {
    // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   if(objetoUsuario.password === password){
+      return true;
+   } else{
+      return false;
+   }
 }
 
 function actualizarPassword(objetoUsuario, nuevaPassword) {
