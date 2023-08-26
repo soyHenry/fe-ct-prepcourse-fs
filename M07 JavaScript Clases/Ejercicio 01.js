83ae71ea52439cf37beef9ae36ea40d8b219c8a2
Nydia Massiel Sánchez <66643813+nydsanchez@users.noreply.github.com>
2023-08-26 12:00:12 -0600

    Update Ejercicio 01.js

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -12,28 +12,43 @@ function crearUsuario() {
    // Tu código:
 
    class Usuario {
-      constructor( nombre, email, password){
-         nombre = nombre;
-         email = email;
-         password = password;
+      constructor( usuario, nombre, email, password){
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
       };
       saludar (){
-         return 'Hola, ni nombre es {{this.nombre}}'
-      }
+         return 'Hola, mi nombre es ' + this.nombre;
+      };
    }
-return Usuario ();
+return Usuario;
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
-}
+
+Usuario.prototype.saludar = function() {
+          return 'Hello World!';
+      };
+   }
+
+
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+
+   String.prototype.reverse = function(){
+      var str= "";
+      for (let i=this.length -1; i>= 0; i--){
+      str += this[i];
+      }
+      return str;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
