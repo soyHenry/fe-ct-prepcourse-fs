fe6e742df0162ea900edcb8255600bc95990ca0a
SalazarProgrammer <000jsalazar@gmail.com>
2023-09-15 12:02:09 -0400

    Primer commit del Curso Henry M03 Js

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -2,43 +2,49 @@
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
 function devolverString(string) {
-   // Debe retornar un string.
-   // Tu código:
+	// Debe retornar un string.
+	// Tu código:
+	return string;
 }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
-   // Retorna el resultado de su suma.
-   // Tu código:
+	// Retorna el resultado de su suma.
+	// Tu código:
+	return x + y;
 }
 
 function resta(x, y) {
-   // Retorna el resultado de la resta.
-   // Tu código:
+	// Retorna el resultado de la resta.
+	// Tu código:
+	return x - y;
 }
 
 function divide(x, y) {
-   // Retorna el resultado de su división.
-   // Tu código:
+	// Retorna el resultado de su división.
+	// Tu código:
+	return x / y;
 }
 
 function multiplica(x, y) {
-   // Retorna el resultado de su multiplicación.
-   // Tu código:
+	// Retorna el resultado de su multiplicación.
+	// Tu código:
+	return x * y;
 }
 
 function obtenerResto(x, y) {
-   // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+	// Obtén el resto de la división de "x" entre "y".
+	// Tu código:
+	return x % y;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   devolverString,
-   suma,
-   resta,
-   divide,
-   multiplica,
-   obtenerResto,
+	devolverString,
+	suma,
+	resta,
+	divide,
+	multiplica,
+	obtenerResto,
 };
