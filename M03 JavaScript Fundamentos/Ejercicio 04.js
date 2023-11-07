97a5c492995086bbb56123f2bf10ce3f9ce1dce7
HenryMauricio <hmauricio1987@gmail.com>
2023-11-06 21:02:53 -0500

    Tarea fundamentos

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -4,31 +4,38 @@
 // ⛔️ Recuerda que debes utilizar el objeto global "Math".
 
 function elevarAlCuadrado(num) {
+   return Math.pow(num, 2);
+   
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
 }
 
 function elevarAlCubo(num) {
+   return Math.pow(num, 3);
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
 }
 
 function elevar(num, exponent) {
+   return Math.pow(num, exponent);
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
 }
 
 function redondearNumero(num) {
+   return Math.round(num);
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
 }
 
 function redondearHaciaArriba(num) {
+   return Math.ceil(num);
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
 }
 
 function numeroRandom() {
+   return Math.random();
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
 }
