4d10bf5b7b3bc20ac38202d6b70ab6ebbb22f747
AleOmi98 <alejandro_199841@outlook.es>
2023-11-16 19:37:23 -0300

    Modulo 7 completado

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -2,29 +2,48 @@
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
 class Persona {
-   // Crea el constructor de la clase "Persona".
-   // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
-   // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
-   // sus valores.
+  // Crea el constructor de la clase "Persona".
+  // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
+  // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
+  // sus valores.
+  constructor(nombre, apellido, edad, domicilio) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.domicilio = domicilio;
+  }
 }
+
+Persona.prototype.detalle = function (detalles) {
+  return {
+    nombre: this.nombre,
+    apellido: this.apellido,
+    edad: this.edad,
+    domicilio: this.domicilio,
+  };
+};
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
-   // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
-   // Recibirás las propiedades por parámetro.
-   // Retornar la instancia creada.
-   // Tu código:
+  // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
+  // Recibirás las propiedades por parámetro.
+  // Retornar la instancia creada.
+  // Tu código:
+  const nuevaPersona = new Persona(nombre, apellido, edad, domicilio);
+  return nuevaPersona;
 }
-
 function agregarMetodo() {
-   // La función agrega un método "datos" a la clase "Persona".
-   // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
-   // Ejemplo: "Juan, 22 años".
-   // Tu código:
+  // La función agrega un método "datos" a la clase "Persona".
+  // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
+  // Ejemplo: "Juan, 22 años".
+  // Tu código:
+  Persona.prototype.datos = function () {
+    return this.nombre + ", " + this.edad + " años";
+  };
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   crearInstanciaPersona,
-   agregarMetodo,
-   Persona,
+  crearInstanciaPersona,
+  agregarMetodo,
+  Persona,
 };
