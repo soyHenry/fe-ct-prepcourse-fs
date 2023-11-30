f2cd63a2f4565a08853c1b470c1e5ffe71b02c5c
Jose Granero <josegranero756@gmail.com>
2023-11-29 21:26:26 -0300

    "terminado el modulo 3"

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,22 +5,22 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-var nuevoString = 'Hola';
+const nuevoString = 'Hola';
 
 // Crea una variable de tipo number.
-var nuevoNumero = 5;
+const nuevoNumero = 5;
 
 // Crea una variable de tipo boolean.
-var nuevoBoolean = true;
+const nuevoBoolean = true;
 
 // Resuelve el siguiente problema matemático.
-var nuevaResta = 10 - 7 == 3;
+const nuevaResta = 10 - 7 == 3;
 
 // Resuelve el siguiente problema matemático.
-var nuevaMultiplicacion = 10 * 4 == 40;
+const nuevaMultiplicacion = 10 * 4 == 40;
 
 // Resuelve el siguiente problema matemático.
-var nuevoModulo = 21 % 5 === 1 ;
+const nuevoModulo = 21 % 5 === 1 ;
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
