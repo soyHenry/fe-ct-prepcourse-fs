a92fbb1d3138334badb2f79fecf4b3e56c4c086c
henrymauricio1987 <hmauricio1987@gmail.com>
2024-02-08 10:01:52 -0500

    En Barranquilla me quedo

--- M07 Arrays/ejercicios/03.js
+++ M07 Arrays/ejercicios/03.js
@@ -2,13 +2,14 @@ function encontrarElemento(elemento, array) {
   // Busca el elemento pasado por argumento dentro del array.
   // Si no se encuentra, retorna -1.
   // Tu código:
+  for (var i = 0; i < array.length ; i++){
+    if (array [i] === elemento){
+      return i;
+    }
+  }
+    return -1;
 }
 
-for (i = 0; i < array.length ; i++){
-  if (array [i] === elemento){
-    return i;
-  }
-  return -1;
-}
+
 
 module.exports = encontrarElemento;
