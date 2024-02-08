73a5dd1e02fae8c42b4eabb153a1742ae5f8d5e8
henrymauricio1987 <hmauricio1987@gmail.com>
2024-02-08 10:40:47 -0500

    Ejercicio metodos nuevos

--- M07 Arrays/ejercicios/04.js
+++ M07 Arrays/ejercicios/04.js
@@ -2,6 +2,12 @@ function obtenerElementoAleatorio(array) {
   // Devuelve un elemento aleatorio del arreglo array.
   // PISTA: Usa el método Math.random().
   // Tu código:
+
+  var indiceAleatorio = Math.floor(Math.random() * array.length);
+  
+  // Devolver el elemento del arreglo en el índice aleatorio
+  return array[indiceAleatorio];
+  
 }
 
 module.exports = obtenerElementoAleatorio;
--- M07 Arrays/ejercicios/05.js
+++ M07 Arrays/ejercicios/05.js
@@ -1,6 +1,20 @@
 function obtenerPrimerStringLargo(array) {
   // Devuelve el primer string con más de 5 caracteres en el array.
   // Tu código:
+
+  console.log(obtenerPrimerStringLargo);
+
+  var temporal = undefined;
+
+  for (var i = 0; i < array.length ; i++ ){
+    if (array[i].length >= 5){
+      temporal = array[i]
+      break;
+  }
 }
+return temporal;
+}
+
+obtenerPrimerStringLargo ["Barranquilla", "Holas", "Ciudad"]
 
 module.exports = obtenerPrimerStringLargo;
