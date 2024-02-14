8f604f2ab8fb67d135387632f26ba3216c77cf7a
thayrov <thayrovg@gmail.com>
2024-02-14 00:09:42 -0600

    🐛 [FIX]: Corrige imports en tests del Módulo 7
    Este commit corrige los imports incorrectos en los archivos M7T4.test.js y M7T5.test.js, asegurando que las pruebas unitarias referencien las funciones correctas. Se ajustan los imports para apuntar a los ejercicios correctos, mejorando la precisión de las pruebas y evitando errores de referencia.

--- C7 Arrays/tests/M7T4.test.js
+++ C7 Arrays/tests/M7T4.test.js
@@ -1,11 +1,15 @@
-const agregarItemAlFinalDelArray = require('../ejercicios/03');
+const agregarItemAlFinalDelArray = require('../ejercicios/04');
 
 test('Debe retornar el arreglo con el elemento agregado al final', function () {
-   expect(agregarItemAlFinalDelArray([10, 10, 16, 12], 10)).toEqual([
-      10, 10, 16, 12, 10,
-   ]);
-   expect(
-      agregarItemAlFinalDelArray([97, 100, 80, 55, 72, 94], 'Hola')
-   ).toEqual([97, 100, 80, 55, 72, 94, 'Hola']);
-   expect(agregarItemAlFinalDelArray([], true)).toEqual([true]);
+  expect(agregarItemAlFinalDelArray([10, 10, 16, 12], 10)).toEqual([10, 10, 16, 12, 10]);
+  expect(agregarItemAlFinalDelArray([97, 100, 80, 55, 72, 94], 'Hola')).toEqual([
+    97,
+    100,
+    80,
+    55,
+    72,
+    94,
+    'Hola',
+  ]);
+  expect(agregarItemAlFinalDelArray([], true)).toEqual([true]);
 });
