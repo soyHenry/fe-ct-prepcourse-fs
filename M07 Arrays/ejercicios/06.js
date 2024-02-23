33ec45003bdd597bbe6c8bbc2d40b7236c12ba11
henrymauricio1987 <hmauricio1987@gmail.com>
2024-02-23 18:27:23 -0500

    Julio Mario SantoDomingo

--- M07 Arrays/ejercicios/06.js
+++ M07 Arrays/ejercicios/06.js
@@ -2,6 +2,12 @@ function duplicarElementos(array) {
   // Duplica (multiplica x2) cada elemento del array de números.
   // Devuelve un array con los duplicados.
   // Tu código:
+
+  var mult = i * 2;
+  for (var i = 0 ; i < array.length ; i++){
+    if (mult < array[i].length )
+    return mult * array;
+  }
 }
 
 module.exports = duplicarElementos;
