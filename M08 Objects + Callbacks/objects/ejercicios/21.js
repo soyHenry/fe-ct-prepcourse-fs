ac36b8a5872e82a48cac90ead1176ff2b062ee49
henrymauricio1987 <hmauricio1987@gmail.com>
2024-03-04 17:10:48 -0500

    Torres de Mediterraneo

--- M08 Objects + Callbacks/objects/ejercicios/21.js
+++ M08 Objects + Callbacks/objects/ejercicios/21.js
@@ -9,6 +9,14 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
   // PorcentajeDeDescuento ---> 0.2
   // Precio final ---> 8
   // Tu código:
+
+  objetoProducto.calcularPrecioDescuento = function(){
+    return{
+      objetoProducto.precio -
+      objetoProducto.precio * objetoProducto.porcentajeDeDescuento
+    };
+  };
+  return objetoProducto;
 }
 
 module.exports = agregarMetodoCalculoDescuento;
