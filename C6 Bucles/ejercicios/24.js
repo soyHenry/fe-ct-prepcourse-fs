42c3ce6913f3a7e3e05a3d9ff0e6b4289271a7fc
Camilo Pineda <b.camilo.15@gmail.com>
2024-04-02 20:15:19 -0300

    C6 Bucles

--- C6 Bucles/ejercicios/24.js
+++ C6 Bucles/ejercicios/24.js
@@ -1,7 +1,11 @@
-function invertirTexto(texto) {
-   // La función recibe un argumento "texto" que es un string.
-   // Tu objetivo es invertir el string y devolver el string invertido.
-   // Tu código:
+function esNumeroPrimo(numero) {
+  // La función recibe un argumento "numero".
+  // Determina si este corresponde a un número primo.
+  // De ser así, retorna true.
+  // De lo contrario, retorna false.
+  // IMPORTANTE: Recuerda que un número primo es aquel que
+  // solo es divisible por sí mismo y por 1.
+  // Tu código:
 }
 
-module.exports = invertirTexto;
+module.exports = esNumeroPrimo;
